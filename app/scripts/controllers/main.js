4b12126cf6b3050ebd6a8be9c50e0a03c140c114
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2013-11-25 00:58:46 +0530

    List mps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('MeraMinisterApp')
-  .controller('MainCtrl', function ($scope, Ministry) {
-    Ministry.states().then(function(found){
-      $scope.states = found;
-    })
-  });
+    .controller('MainCtrl', function ($scope, Ministry) {
+      Ministry.states().then(function (found) {
+        $scope.states = found;
+      });
+
+      $scope.getMinisters = function(stateId){
+        Ministry.mps(stateId).then(function(found){
+          $scope.ministers = found;
+        })
+      }
+    });
--- app/scripts/services/Ministry.js
+++ app/scripts/services/Ministry.js
@@ -3,8 +3,11 @@
 angular.module('MeraMinisterApp')
     .service('Ministry', ['$http', '$q', 'MyMinisterInfo', function ($http, $q, MyMinisterInfo) {
       return {
-        states: function(){
-          return MyMinisterInfo.call('/states.json');
+        states: function () {
+          return MyMinisterInfo.call('states.json');
+        },
+        mps: function (stateId) {
+          return MyMinisterInfo.call("states/" + stateId + "/mps.json");
         }
       }
     }]);
