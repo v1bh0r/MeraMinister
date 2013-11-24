2baa702ee03af690b31ea281a1ca96367a8cd293
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2013-11-24 23:50:51 +0530

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('MeraMinisterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
