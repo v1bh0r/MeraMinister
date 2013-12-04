f98beee93e4370f2533c1f6023877798db15162d
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2013-12-04 19:34:58 +0530

    Fetch news

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,22 @@
 'use strict';
 
 angular.module('MeraMinisterApp')
-    .controller('MainCtrl', function ($scope, Ministry) {
+    .controller('MainCtrl', function ($scope, Ministry, googleFeeds) {
       Ministry.states().then(function (found) {
         $scope.states = found;
       });
 
-      $scope.getMinisters = function(stateId){
-        Ministry.mps(stateId).then(function(found){
+      $scope.getMinisters = function (stateId) {
+        Ministry.mps(stateId).then(function (found) {
           $scope.ministers = found;
-        })
+        });
+      }
+
+      $scope.news = null;
+
+      $scope.loadNews = function (minister) {
+        googleFeeds.fetchFeed('https://news.google.com/news?hl=en&as_occt=any&as_qdr=y&q=' + minister.name + '&um=1&ie=UTF-8&output=rss').then(function (results) {
+          $scope.news = results;
+        });
       }
     });
--- app/scripts/services/MyMinisterInfo.js
+++ app/scripts/services/MyMinisterInfo.js
@@ -1,17 +1,11 @@
 'use strict';
 
 angular.module('MeraMinisterApp')
-    .service('MyMinisterInfo', ['$http', '$q', function ($http, $q) {
+    .service('MyMinisterInfo', ['$http', '$q', 'JsonP', function ($http, $q, JsonP) {
       var BASE_URL = "http://api.myminister.info/";
       return {
         call: function (relativeUrl) {
-          var deferred = $q.defer();
-          $http({method: 'JSONP', url: BASE_URL + relativeUrl + "?callback=JSON_CALLBACK", cache: true}).then(function (result) {
-            deferred.resolve(JSON.parse(result.data.model));
-          }, function (problem) {
-            deferred.reject(problem)
-          });
-          return deferred.promise;
+          return JsonP.fetch(BASE_URL + relativeUrl);
         }
       }
     }]);
